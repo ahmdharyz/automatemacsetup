97147317103da55bd4df80ab37cafd8a65485851
haryz <ahmed.haryz@gmail.com>
2022-05-19 00:03:46 +0500

    Create brew.sh
    brew.sh contains currently used applications

---
+++ brew.sh
@@ -0,0 +1,26 @@
+brew install python3
+brew install tree
+
+brew install --cask 1password
+brew install --cask android-file-transfer
+brew install --cask android-messages
+brew install --cask daisydisk
+brew install --cask connectiq
+brew install --cask google-chrome
+brew install --cask google-drive
+brew install --cask microsoft-excel
+brew install --cask microsoft-word
+brew install --cask microsoft-remote-desktop
+brew install --cask microsoft-teams
+brew install --cask mipony
+brew install --cask monitor control
+brew install --cask mounty
+brew install --cask numi
+brew install --cask onedrive
+brew install --cask telegram
+brew install --cask todoist
+brew install --cask transmission
+brew install --cask viber
+brew install --cask vlc
+brew install --cask visual-studio-code
+brew install --cask whatsapp
